660fd8c8b74393b087a326d9fb1cb3d3e57ddb1e
rwasef1830 <beliskner.github.3psil0N@neverbox.com>
2017-03-21 01:56:54 +0200

    Migrate solution to VS2017. Use NUnitLite for test project until NUnit gains .NET Standard support.

---
+++ test/ProjectVersioning.DotNet.Cli.Tests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using NUnitLite;
+
+namespace ProjectVersioning.DotNet.Cli.Tests
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    static class Program
+    {
+        static int Main(string[] args)
+        {
+            return new AutoRun(typeof(Program).GetTypeInfo().Assembly).Execute(args);
+        }
+    }
+}
